49db822929a77d883c9fdd87216908390770f6df
Yousef <youseflkc@yahoo.ca>
2022-05-22 19:51:09 -0400

    added cors origin

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -76,7 +76,8 @@ INTERNAL_IPS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
-    'http://127.0.0.1:8001'
+    'http://localhost:4200',
+    'http://127.0.0.1:8001',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
