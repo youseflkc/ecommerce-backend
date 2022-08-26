5f0d4dabe657013194e987e3a060bf2bb90dc1a4
Yousef <youseflkc@yahoo.ca>
2022-08-26 18:43:36 -0400

    changed image url char max_length

--- store/models.py
+++ store/models.py
@@ -49,7 +49,7 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.URLField(max_length=255)
+    image = models.URLField(max_length=1024)
 
 
 class Customer(models.Model):
